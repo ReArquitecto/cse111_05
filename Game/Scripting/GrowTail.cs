6545e4f2295eace6e3c87f629e6e669061e879ff
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2022-12-01 09:36:59 -0700

    Grow tail

---
+++ Game/Scripting/GrowTail.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using Unit05.Game.Casting;
+using Unit05.Game.Services;
+
+
+namespace Unit05.Game.Scripting
+{
+    /// <summary>
+    /// <para>An output action that draws all the actors.</para>
+    /// <para>The responsibility of DrawActorsAction is to draw each of the actors.</para>
+    /// </summary>
+    public class GrowTail : Action
+    {
+        private VideoService _videoService;
+
+        /// <summary>
+        /// Constructs a new instance of ControlActorsAction using the given KeyboardService.
+        /// </summary>
+
+        /// <inheritdoc/>
+        public void GrowTailScript(Cast cast, Script script)
+        {
+            Snake snake1 = (Snake)cast.GetFirstActor("snake1");
+            Snake snake2 = (Snake)cast.GetFirstActor("snake2");
+            List<Actor> segments = snake1.GetSegments();
+            List<Actor> segments2 = snake2.GetSegments();
+            List<Actor> messages = cast.GetActors("messages");
+            
+            _videoService.ClearBuffer();
+            _videoService.DrawActors(segments);
+            _videoService.DrawActors(segments2);
+            _videoService.DrawActors(messages);
+            _videoService.FlushBuffer();
+        }
+    }
+}
